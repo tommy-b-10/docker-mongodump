0b3dc93028187b8a51c4607df7ea1d385e6ef024
tommy-b-10 <tommy-b-10@hotmail.com>
2020-06-10 22:07:53 +0930

    Create dir if does not exist

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ set -e
 echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
-
+ls /backup/$MONGO_NAME 2>/dev/null||mkdir /backup/$MONGO_NAME
 FILE="/backup/$MONGO_NAME/backup-$DATE.tar.gz"
 mkdir -p dump
 mongodump --uri "$MONGO_URI"
@@ -13,6 +13,7 @@ tar -zcvf "$FILE" dump/
 rm -rf dump/
 
 if [[ "$MONGO_URI_2" ]]; then
+    ls /backup/$MONGO_NAME_2 2>/dev/null||mkdir /backup/$MONGO_NAME_2
     FILE="/backup/$MONGO_NAME_2/backup-$DATE.tar.gz"
     mkdir -p dump
     mongodump --uri "$MONGO_URI_2"
@@ -20,6 +21,7 @@ if [[ "$MONGO_URI_2" ]]; then
     rm -rf dump/
 fi
 if [[ "$MONGO_URI_3" ]]; then
+    ls /backup/$MONGO_NAME_3 2>/dev/null||mkdir /backup/$MONGO_NAME_3
     FILE="/backup/$MONGO_NAME_3/backup-$DATE.tar.gz"
     mkdir -p dump
     mongodump --uri "$MONGO_URI_3"
@@ -27,6 +29,7 @@ if [[ "$MONGO_URI_3" ]]; then
     rm -rf dump/
 fi
 if [[ "$MONGO_URI_4" ]]; then
+    ls /backup/$MONGO_NAME_4 2>/dev/null||mkdir /backup/$MONGO_NAME_4
     FILE="/backup/$MONGO_NAME_4/backup-$DATE.tar.gz"
     mkdir -p dump
     mongodump --uri "$MONGO_URI_4"
