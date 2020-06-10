a9d8ea57345049965b98483bc12d015254b30bf8
tommy-b-10 <tommy-b-10@hotmail.com>
2020-06-10 22:35:45 +0930

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,35 +13,19 @@ export MONGO_NAME_4=$MONGO_NAME_4
 
 # Optional env vars:
 # - CRON_SCHEDULE
-# - TARGET_S3_FOLDER
-# - AWS_ACCESS_KEY_ID
-# - AWS_SECRET_ACCESS_KEY
 
 if [[ "$CRON_SCHEDULE" ]]; then
     LOGFIFO='/var/log/cron.fifo'
     if [[ ! -e "$LOGFIFO" ]]; then
         mkfifo "$LOGFIFO"
     fi
-    CRON_ENV="MONGO_URI='$MONGO_URI'"
-    echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
-    
-    if [[ "$MONGO_URI_2" ]]; then
-        echo -e "$MONGO_URI_2\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
-    fi
-    
-    if [[ "$MONGO_URI_3" ]]; then
-        echo -e "$MONGO_URI_3\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
-    fi
-    if [[ "$MONGO_URI_4" ]]; then
-        echo -e "$MONGO_URI_4\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
-    fi
-    
+
+    (crontab -l 2>/dev/null; echo -e "$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1") | crontab -
+
     if [[ "$REMOVE_AFTER" ]]; then
-        echo -e "@daily find /backup/* -mtime +$REMOVE_AFTER -type f -delete > $LOGFIFO 2>&1" | crontab -
+        (crontab -l 2>/dev/null; echo -e "0 4 * * * find /backup/* -mtime +$REMOVE_AFTER -type f -delete > $LOGFIFO 2>&1") | crontab -
     fi
-    
-    crontab -l
-    cron
+
     tail -f "$LOGFIFO"
 else
     exec /backup.sh
