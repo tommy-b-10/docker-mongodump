b72c167847e62ff9112e4a0f538ee5baf3bbf650
tommy-b-10 <tommy-b-10@hotmail.com>
2020-06-10 21:34:17 +0930

    Added support for 4x MongoDBs

--- backup.sh
+++ backup.sh
@@ -5,17 +5,33 @@ set -e
 echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
-FILE="/backup/backup-$DATE.tar.gz"
 
+FILE="/backup/$MONGO_NAME-backup-$DATE.tar.gz"
 mkdir -p dump
 mongodump --uri "$MONGO_URI"
 tar -zcvf "$FILE" dump/
 rm -rf dump/
 
-if [[ "$TARGET_S3_FOLDER" ]]; then
-    aws s3 cp "$FILE" "$TARGET_S3_FOLDER"
-    echo "$FILE uploaded to $TARGET_S3_FOLDER"
-    rm -rf "$FILE"
+if [[ "$MONGO_URI_2" ]]; then
+    FILE="/backup/$MONGO_NAME_2-backup-$DATE.tar.gz"
+    mkdir -p dump
+    mongodump --uri "$MONGO_URI_2"
+    tar -zcvf "$FILE" dump/
+    rm -rf dump/
+fi
+if [[ "$MONGO_URI_3" ]]; then
+    FILE="/backup/$MONGO_NAME_3-backup-$DATE.tar.gz"
+    mkdir -p dump
+    mongodump --uri "$MONGO_URI_3"
+    tar -zcvf "$FILE" dump/
+    rm -rf dump/
+fi
+if [[ "$MONGO_URI_4" ]]; then
+    FILE="/backup/$MONGO_NAME_4-backup-$DATE.tar.gz"
+    mkdir -p dump
+    mongodump --uri "$MONGO_URI_4"
+    tar -zcvf "$FILE" dump/
+    rm -rf dump/
 fi
 
 echo "Job finished: $(date)"
