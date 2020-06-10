ae0f79676750c21503e90808a278a36dfea32cd9
tommy-b-10 <tommy-b-10@hotmail.com>
2020-06-10 21:55:48 +0930

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,28 +6,28 @@ echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
 
-FILE="/backup/$MONGO_NAME-backup-$DATE.tar.gz"
+FILE="/backup/$MONGO_NAME/backup-$DATE.tar.gz"
 mkdir -p dump
 mongodump --uri "$MONGO_URI"
 tar -zcvf "$FILE" dump/
 rm -rf dump/
 
 if [[ "$MONGO_URI_2" ]]; then
-    FILE="/backup/$MONGO_NAME_2-backup-$DATE.tar.gz"
+    FILE="/backup/$MONGO_NAME_2/backup-$DATE.tar.gz"
     mkdir -p dump
     mongodump --uri "$MONGO_URI_2"
     tar -zcvf "$FILE" dump/
     rm -rf dump/
 fi
 if [[ "$MONGO_URI_3" ]]; then
-    FILE="/backup/$MONGO_NAME_3-backup-$DATE.tar.gz"
+    FILE="/backup/$MONGO_NAME_3/backup-$DATE.tar.gz"
     mkdir -p dump
     mongodump --uri "$MONGO_URI_3"
     tar -zcvf "$FILE" dump/
     rm -rf dump/
 fi
 if [[ "$MONGO_URI_4" ]]; then
-    FILE="/backup/$MONGO_NAME_4-backup-$DATE.tar.gz"
+    FILE="/backup/$MONGO_NAME_4/backup-$DATE.tar.gz"
     mkdir -p dump
     mongodump --uri "$MONGO_URI_4"
     tar -zcvf "$FILE" dump/
