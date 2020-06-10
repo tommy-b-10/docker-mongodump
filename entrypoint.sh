b08eb15f01fcb333ff218e446e855857af2fc598
tommy-b-10 <tommy-b-10@hotmail.com>
2020-06-10 21:31:18 +0930

    Remove AWS support, add support for 4 individual MongoDB's

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,16 +16,19 @@ if [[ "$CRON_SCHEDULE" ]]; then
         mkfifo "$LOGFIFO"
     fi
     CRON_ENV="MONGO_URI='$MONGO_URI'"
-    if [[ "$TARGET_S3_FOLDER" ]]; then
-        CRON_ENV="$CRON_ENV\nTARGET_S3_FOLDER='$TARGET_S3_FOLDER'"
-    fi
-    if [[ "$AWS_ACCESS_KEY_ID" ]]; then
-        CRON_ENV="$CRON_ENV\nAWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID'"
-    fi
-    if [[ "$AWS_SECRET_ACCESS_KEY" ]]; then
-        CRON_ENV="$CRON_ENV\nAWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY'"
-    fi
     echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
+    
+    if [[ "$MONGO_URI_2" ]]; then
+        echo -e "$MONGO_URI_2\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
+    fi
+    
+    if [[ "$MONGO_URI_3" ]]; then
+        echo -e "$MONGO_URI_3\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
+    fi
+    if [[ "$MONGO_URI_4" ]]; then
+        echo -e "$MONGO_URI_4\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
+    fi
+    
     crontab -l
     cron
     tail -f "$LOGFIFO"
