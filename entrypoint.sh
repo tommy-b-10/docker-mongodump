048a0c864904e3eb051fca7b38a8684f29f7fc61
tommy-b-10 <tommy-b-10@hotmail.com>
2020-06-12 13:36:10 +0930

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,22 +20,28 @@ if [[ "$CRON_SCHEDULE" ]]; then
     if [[ ! -e "$LOGFIFO" ]]; then
         mkfifo "$LOGFIFO"
     fi
-    echo -e "#BACKUP SCRIPT CRONJOBS\n" > new_cron.txt
+    echo -e "#BACKUP SCRIPT CRONJOBS" > new_cron.txt
     echo "MONGO_URI=$MONGO_URI" >> new_cron.txt
     echo "MONGO_NAME=$MONGO_NAME" >> new_cron.txt
-    echo "MONGO_URI_2=$MONGO_URI_2" >> new_cron.txt
-    echo "MONGO_NAME_2=$MONGO_NAME_2" >> new_cron.txt
-    echo "MONGO_URI_3=$MONGO_URI_3" >> new_cron.txt
-    echo "MONGO_NAME_3=$MONGO_NAME_3" >> new_cron.txt
-    echo "MONGO_URI_4=$MONGO_URI_4" >> new_cron.txt
-    echo "MONGO_NAME_4=$MONGO_NAME_4" >> new_cron.txt
+    if [[ "$MONGO_URI_2" ]]; then
+        echo "MONGO_URI_2=$MONGO_URI_2" >> new_cron.txt
+        echo "MONGO_NAME_2=$MONGO_NAME_2" >> new_cron.txt
+    fi
+    if [[ "$MONGO_URI_3" ]]; then
+        echo "MONGO_URI_3=$MONGO_URI_3" >> new_cron.txt
+        echo "MONGO_NAME_3=$MONGO_NAME_3" >> new_cron.txt
+    fi
+    if [[ "$MONGO_URI_4" ]]; then
+        echo "MONGO_URI_4=$MONGO_URI_4" >> new_cron.txt
+        echo "MONGO_NAME_4=$MONGO_NAME_4" >> new_cron.txt
+    fi
     echo "$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" >> new_cron.txt
 
     if [[ "$REMOVE_AFTER" ]]; then
        echo "0 4 * * * find /backup/* -mtime +$REMOVE_AFTER -type f -delete > $LOGFIFO 2>&1" >> new_cron.txt
     fi
 
-    cron new_cron.txt
+    cat new_cron.txt | crontab -
 
     tail -f "$LOGFIFO"
 else
